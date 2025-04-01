c7950975fb0811d25585744bb78a13aa765e64e3
k-ssu <tndk617@naver.com>
2025-04-02 05:58:53 +0900

    gitignore 수정 및 ec2 접근을 위한 public ip, private key pem output 설정

--- ec2/main.tf
+++ ec2/main.tf
@@ -52,7 +52,7 @@ module "demo_key_pair" {
 
 resource "aws_instance" "demo_instance" {
   ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   subnet_id = module.demo_vpc.public_subnets[0]
   associate_public_ip_address = true
@@ -62,7 +62,8 @@ resource "aws_instance" "demo_instance" {
 
   root_block_device {
     volume_type = "gp2"
-    volume_size = 8
+    # volume 사이즈는 줄일 수 없음.
+    volume_size = 10
   }
 }
 
@@ -74,4 +75,22 @@ data "aws_ami" "ubuntu" {
     name = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+}
+
+# EC2 접근을 위한 pulbic ip, private pem key 확인
+output "public_ip" {
+  value = aws_instance.demo_instance.public_ip
+}
+
+# private key는 sensitive = true 설정 하지 않으면 확인 불가
+output "private_key" {
+    value = module.demo_key_pair.private_key_pem
+    sensitive = true
+/*
+    private key 파일로 생성
+    $ terraform output -json | jq -r .private_key.value > key
+    
+    ec2 접속 
+    $ ssh -i [*.pem] ubuntu@[public ip]
+*/    
 }
